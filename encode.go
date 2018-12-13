8bf96af660d4ec4abdb0d4746072deb2a4a769c4
Eduard Urbach <e.urbach@gmail.com>
2018-12-13 13:00:04 +0900

    Initial commit

---
+++ encode.go
@@ -0,0 +1,5 @@
+package main
+
+func encode(fileIn string, fileOut string) {
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "flag"
 
+func main() {
+	flag.Parse()
+
+	// The command we're going to perform
+	command := flag.Arg(0)
+
+	switch command {
+	case "encode":
+		println("encode")
+	default:
+		help()
+	}
 }
